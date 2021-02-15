195285069c08d313b37d191bf75c961ba588b158
TyurinV <denfal486@gmailc.om>
2021-02-15 22:27:56 +0300

    First Version

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,22 @@
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTest {
+
+    @Test
+    void selenideSearchTest() {
+        // Открыть googlea
+        open("https://www.google.com/");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("#search").shouldHave(text("selenide.org"));
+    }
+}
+
